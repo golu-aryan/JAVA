e4796bd32427ead35d0da13f88c6323d0841b38e
Aryan <kumararyan9852@gmail.com>
2024-05-19 22:20:33 +0530

    java

---
+++ Array/1D_Array/floor.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class floor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        for (int floor = max; floor >= 1; floor--) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] >= floor) {
+                    System.out.print("*\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
