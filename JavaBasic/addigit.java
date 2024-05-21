427548eacb8c5c2940c4feb0de9c07ea4c4483bf
Aryan <kumararyan9852@gmail.com>
2024-05-21 23:14:53 +0530

    java

---
+++ JavaBasic/addigit.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class addigit{
+    public static void main(String[] args){
+        Scanner n = new Scanner(System.in);
+
+        System.out.println("Enter an integer between 0 and 1000: ");
+
+        int num = n.nextInt();
+
+        int firsDdigit = num % 10;
+        int remainingNumber = num / 10;
+        int secondDigit = remainingNumber %10;
+        remainingNumber = remainingNumber / 10;
+        int thirdDigit = remainingNumber % 10;
+        remainingNumber = remainingNumber / 10;
+        int fourthDigit = remainingNumber % 10;
+
+        int sum = thirdDigit + secondDigit + firsDdigit + fourthDigit;
+        System.out.println("The sum of all digits in "+ num + " is " +sum);
+    }
+}
