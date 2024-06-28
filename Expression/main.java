437d2a51d3c524d34b37c80bd56228580a5beff1
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-06-28 18:26:01 +0530

    java

---
+++ Expression/main.java
@@ -0,0 +1,12 @@
+
+public class main{
+    public static void main(String[] args){
+        int friends = 10;
+        friends++;
+
+        System.out.println(friends);        
+    }
+    //expression = operator & operands
+    //operands = values, variables, numbers, quantity
+    //operators = + - * % / ++ -- & ^
+}
