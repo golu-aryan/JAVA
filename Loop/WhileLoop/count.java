e9caa20dbcafac0f7e2d9d45f0a531e20df27f28
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-06-29 15:56:55 +0530

    java

---
+++ Loop/WhileLoop/count.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+//Write a program count the digit which is input by the user.
+public class count{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the digit:");
+        int n = s.nextInt();
+        int count = 0;
+        while(n!=0){
+            n /= 10;
+            count++;
+        }
+        System.out.println("The number of digit is :" + count);
+    }
+}
