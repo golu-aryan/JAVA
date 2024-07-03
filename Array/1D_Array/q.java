3b5307fa5c0be282d4da46d967ff0d7c5f41aaed
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-07-03 15:42:31 +0530

    java

---
+++ Array/1D_Array/q.java
@@ -0,0 +1,13 @@
+public class q {
+    public static void main(String[] args){
+        int[] num = new int[26];
+
+        num[0] = 100;
+        num[25] = 200;
+        int temp = num[25];
+        num[25] = num[0];
+        num[0] = temp;
+
+        System.out.println("\n" + num[0] + " "+ num[25]);
+    }
+}
