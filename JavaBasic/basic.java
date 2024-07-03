ba7782cfbbfcbbf65d0a224fcc5a552186816293
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-07-03 18:24:11 +0530

    java

---
+++ JavaBasic/basic.java
@@ -0,0 +1,11 @@
+
+public class basic{
+    public static void main(String[] args){
+        double x = 3.14;
+        double y = -10.0;
+
+        double z = Math.floor(x);
+
+        System.out.println(z);
+    }
+}
