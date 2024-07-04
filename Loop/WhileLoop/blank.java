127a9bddd079dbec85577d317a73114279ce4f30
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-07-04 17:00:05 +0530

    java

---
+++ Loop/WhileLoop/blank.java
@@ -0,0 +1,18 @@
+//While loop = executes a block of code as long as a it's condition remains true
+
+import java.util.Scanner;
+
+public class blank {
+    public static void main(String[] args){
+        Scanner n = new Scanner(System.in);
+
+        String name = " ";
+
+        while(name.isBlank()){
+            System.out.println("Enter your name :");
+            name = n.nextLine();
+        }
+
+        System.out.println("Hello " + name);
+    }
+}
