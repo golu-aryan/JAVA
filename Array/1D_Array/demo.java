d2806ed324b2c1f9d66a251ee56ede8f2635126f
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-07-05 15:46:20 +0530

    java

---
+++ Array/1D_Array/demo.java
@@ -0,0 +1,13 @@
+public class demo {
+    public static void main(String[] args){
+        //Array - used to store multiple values in a single variable
+
+        String[] cars = {"Camaro","Tesla","Lamborghini","Buggati","Koeinsggeg"};
+
+        cars[0] = "Mustang";
+        
+
+        System.out.println(cars[2]);
+        
+    }
+}
