15a60a3bb5fb71edd0833387f87b497cfef40ecf
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-07-26 06:38:09 +0530

    java

---
+++ String/equal.java
@@ -0,0 +1,11 @@
+public class equal {
+    public static void main(String[] args){
+        String name = "Bro";
+
+        boolean result = name.equals("b ro");
+        boolean result2 = name.equalsIgnoreCase("bro");
+
+        System.out.println(result);
+        System.out.println(result2);
+    }
+}
