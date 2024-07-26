f1d22fbf9af5c409f8f0cace41211e90290c8899
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-07-26 06:37:27 +0530

    java

---
+++ String/length.java
@@ -0,0 +1,23 @@
+public class length {
+    public static void main(String[] args) {
+        
+        String name = "    Aryan   ";
+
+        //int result = name.length();
+
+        //char result2 = name.charAt(0);
+
+        // int result = name.indexOf("y");
+
+        // boolean result = name.isEmpty();
+
+        // String result = name.trim();
+
+        String result = name.replace("n", "nn");
+        
+        
+
+        System.out.println(result);
+
+    }
+}
