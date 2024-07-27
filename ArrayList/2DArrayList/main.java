dc4e484f947b26728b027a8bab0880b136613e62
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-07-27 20:11:52 +0530

    java

---
+++ ArrayList/2DArrayList/main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class main {
+    public static void main(String[] args) {
+
+        ArrayList<ArrayList<String>>bakeryList = new ArrayList();
+        bakeryList.add("Pasta");
+        bakeryList.add("Garlic Bread");
+        bakeryList.add("Donuts");
+
+
+        ArrayList<String> produceList = new ArrayList();
+
+        bakeryList.add("Pasta");
+        bakeryList.add("Garlic Bread");
+        bakeryList.add("Donuts");
+
+
+        ArrayList<String> DdinkList = new ArrayList();
+
+        bakeryList.add("Pasta");
+        bakeryList.add("Garlic Bread");
+        bakeryList.add("Donuts");
+
+        System.out.println(bakeryList.get(0),get(0));
+
+
+    }
+}
