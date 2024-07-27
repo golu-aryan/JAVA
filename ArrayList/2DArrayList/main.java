defd3b68fbd6103eec9051ac35eed5da44a34dcf
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-07-27 22:28:29 +0530

    java

--- ArrayList/2DArrayList/main.java
+++ ArrayList/2DArrayList/main.java
@@ -1,28 +1,35 @@
 import java.util.ArrayList;
 
+//2D ArrayList= a dynamic list of list..you can change the size of these lists during runtime
+
 public class main {
     public static void main(String[] args) {
 
-        ArrayList<ArrayList<String>>bakeryList = new ArrayList();
+        ArrayList<ArrayList<String>> groceryList = new ArrayList<>();
+
+        ArrayList<String> bakeryList = new ArrayList();
         bakeryList.add("Pasta");
         bakeryList.add("Garlic Bread");
         bakeryList.add("Donuts");
 
 
         ArrayList<String> produceList = new ArrayList();
+        produceList.add("Tomatos");
+        produceList.add("Peppers");
+        produceList.add("Samosas");
 
-        bakeryList.add("Pasta");
-        bakeryList.add("Garlic Bread");
-        bakeryList.add("Donuts");
 
+        ArrayList<String> DrinkList = new ArrayList();
 
-        ArrayList<String> DdinkList = new ArrayList();
+        DrinkList.add("Soda");
+        DrinkList.add("Coffee");
+        DrinkList.add("Juice");
 
-        bakeryList.add("Pasta");
-        bakeryList.add("Garlic Bread");
-        bakeryList.add("Donuts");
+        groceryList.add(bakeryList);
+        groceryList.add(produceList);
+        groceryList.add(DrinkList);
 
-        System.out.println(bakeryList.get(0),get(0));
+        System.out.println(groceryList.get(2));
 
 
     }
