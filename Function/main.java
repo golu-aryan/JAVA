b2202ca32006c087e9072c0e5cdfd87c126ad930
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-07-30 13:05:00 +0530

    java

---
+++ Function/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        String name = "Bro";
+        int age = 21;
+        hello(name, age);
+    }
+
+    static void hello(String title, int age) {
+        System.out.println("Hello" + title + " Your age is" + age);
+    }
+}
