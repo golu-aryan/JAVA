d6768fff14940dc79bc650e553fccedf1e8bab7e
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-08-05 10:55:41 +0530

    java

---
+++ Expression/fin.java
@@ -0,0 +1,8 @@
+public class fin {
+    public static void main(String[] args){
+        //if final keyword use then you cannot change the value of final variable 
+        final double pi = 3.14159;
+
+        System.out.println(pi);
+    }
+}
