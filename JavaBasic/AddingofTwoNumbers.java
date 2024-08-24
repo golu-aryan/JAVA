ab6407a5e8a3a159604feb51e54dbf93c8e930e0
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-08-24 07:58:02 +0530

    java

---
+++ JavaBasic/AddingofTwoNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AddingofTwoNumbers
+{
+    public static void main(String[] args){
+        int num1, num2, add;
+        Scanner n = new Scanner(System.in);
+
+        System.out.print("Enter first number:");
+        num1 = n.nextInt();
+
+        System.out.println("Enter second number:");
+        num2 = n.nextInt();
+
+        add = num1+num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is :" + add);
+
+        n.close();
+    }
+}
