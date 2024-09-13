e23553a95cf62f7df557bf79c3f38bebf6e57021
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-09-14 05:19:58 +0530

    java

---
+++ OOPS/Class_Objects/Arraytosum.java
@@ -0,0 +1,21 @@
+
+class Print{
+    void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
+public class Arraytosum {
+    public static void main(String[] args){
+        int[] myArray = {1,2,3,4,5};
+
+        System.out.println("Array elements are :");
+        Print print = new Print();
+        print.printArray(myArray);
+        
+    }
+
+}
