8fd24220c62aa94c6feaf5d49acda3de34bbc1d6
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-09-15 06:39:05 +0530

    java

--- If-Else/attend.java
+++ If-Else/attend.java
@@ -6,16 +6,21 @@ public class attend {
         Scanner n = new Scanner(System.in);
         char Attendance;
 
-        System.out.print("Name : ");
-        String name = n.nextLine().toUpperCase();
+        System.out.print("Enter the Name : ");
+        String name = n.nextLine();
 
-        System.out.printf("Is %s Present or Absent? Enter value in [P/A] :-", name);
+        System.out.print("Is " + name + " Present or Absent? Enter value in [P/A] :-");
         Attendance = n.nextLine().charAt(0);
-
+        // == - We can use this operator to compare the values of primitive data type
+        // like int, float, char etc
+        // In the case of String == will not compare value of the two String variables.
+        // To compare the values of String we can use String.equals(String) method.
         if (Attendance == 'P' || Attendance == 'p') {
-            System.out.printf("Talk to %s \n", name);
+            System.out.print("Talk to " + name);
+        } else if (Attendance == 'A' || Attendance == 'a') {
+            System.out.print("No Need to talk to " + name);
         } else {
-            System.out.printf("No Need to talk to %s", name);
+            System.out.println("Invalid choice!");
         }
         n.close();
     }
