9fb05a20a71880de9824f19ebbbc400ae14b259f
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-09-16 08:45:00 +0530

    java

---
+++ JavaBasic/GuessOutput.java
@@ -0,0 +1,17 @@
+public class GuessOutput {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = 30;
+
+        if (x < y) {
+            y = x + z;
+        } else {
+            z = y - x;
+        }
+        
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("z: " + z);
+    }
+}
