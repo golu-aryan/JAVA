5440a3c59a175ec42c12ca0140c126714fbdbc86
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-09-19 09:37:57 +0530

    java

--- If-Else/leapyear.java
+++ If-Else/leapyear.java
@@ -1,17 +1,17 @@
-
 import java.util.Scanner;
 
 public class leapyear {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter the year:--");
-        int y;
-        y = s.nextInt();
-        if (y % 4 == 0) {
-            System.err.println("this is leap year!");
-        } else {
-            System.out.println("Sry! this is not leap year");
-        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the year:--");
+        int year = scanner.nextInt();
+        
 
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.err.println(year + " is leap year");
+        } else {
+            System.out.println("Sry!" + year + " This is not leap year");
+        }
+        scanner.close();
     }
 }
