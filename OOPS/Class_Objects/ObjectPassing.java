3bef615b0d6848ec6932e2f4cae3dac2c0334326
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-09-25 13:43:07 +0530

    java

---
+++ OOPS/Class_Objects/ObjectPassing.java
@@ -0,0 +1,25 @@
+class Car {
+    String name;
+
+    Car(String name) {
+        this.name = name;
+    }
+}
+
+class Garage {
+
+    void park(Car car) {
+        System.out.println("The " + car.name + " is parked in the garage");
+    }
+}
+
+public class ObjectPassing {
+    public static void main(String[] args) {
+        Garage garage = new Garage();
+        Car car1 = new Car("BMW");
+        Car car2 = new Car("Tesla");
+
+        garage.park(car1);
+        garage.park(car2);
+    }
+}
