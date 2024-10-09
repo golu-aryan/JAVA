d0ae4e536b12a14cc6dc32778911ec22bbd645f2
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-10-09 06:41:44 +0530

    java

---
+++ Loop/WhileLoop/excontinue.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class excontinue {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num <= 10) {
+            num++;
+            if (num % 3 == 0) {
+                continue;
+            }
+            System.out.println(num);
+        }
+
+    }
+}
