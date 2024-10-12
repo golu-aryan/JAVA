5a8dd06aa8603604d09df9ba28abe098d107c72d
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-10-12 06:07:28 +0530

    java

---
+++ Expression/hi.java
@@ -0,0 +1,12 @@
+
+public class hi{
+    public static void main(String[] args){
+        int friends = 10;
+        friends++;
+
+        System.out.println(friends);        
+    }
+    //expression = operator & operands
+    //operands = values, variables, numbers, quantity
+    //operators = + - * % / ++ -- & ^
+}
