c01a2920dee51ac4ad857f5e4518e12f69a401ec
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-10-16 10:05:00 +0530

    java

---
+++ Loop/WhileLoop/printnumber.java
@@ -0,0 +1,19 @@
+public class printnumber {
+    public static void main(String[] args) {
+        int num = 0;
+        int num1 = 250;
+        int num2 = 300;
+        int num3 = 400;
+        while (num <= 10) {
+            System.out.println(num1);
+            System.out.println(num2);
+            System.out.println(num3);
+            ++num1;
+            ++num2;
+            ++num3;
+
+            ++num;
+        }
+
+    }
+}
