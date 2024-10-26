7df0e25ab86ea4e6b4251d9dc72237b0a069e038
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-10-26 13:02:41 +0530

    java

---
+++ Loop/ForLoop/even1to20.java
@@ -0,0 +1,10 @@
+public class even1to20 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+            else
+                System.out.println(i);
+        }
+    }
+}
