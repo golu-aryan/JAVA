4b8f5940b1b624f60f52f24c14663395c6da4a3c
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-10-27 23:05:24 +0530

    java

--- Loop/ForLoop/sumofn.java
+++ Loop/ForLoop/sumofn.java
@@ -1,19 +1,16 @@
-/*write a program to input the number by the user and print sum of their numbers
- */
-
 import java.util.Scanner;
 
 public class sumofn {
     public static void main(String[] args) {
         int n, sum = 0, i;
-        Scanner t = new Scanner(System.in);
-        System.out.println("Enter the number:");
-        n = t.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        n = scanner.nextInt();
 
         for (i = 1; i <= n; i++) {
-            sum = sum + i;
+            sum += i;
         }
         System.out.println("The Sum of " + n + " Number are :" + sum);
-        t.close();
+        scanner.close();
     }
 }
