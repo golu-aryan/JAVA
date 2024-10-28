f5d1f052a36c6533bec097fe4739b72badb53570
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-10-28 11:16:05 +0530

    java

---
+++ Loop/ForLoop/InvertedPyramid.java
@@ -0,0 +1,17 @@
+public class InvertedPyramid {
+    public static void main(String[] args) {
+
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 4; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
