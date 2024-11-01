1c01dc03474e4561375bc8855c5a5aec4872857f
kumar <122105281+golu-aryan@users.noreply.github.com>
2024-11-01 09:54:35 +0530

    java

---
+++ Loop/ForLoop/allprimenumbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class allprimenumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number :-> ");
+        int n = scanner.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % 2 == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(i + " ");
+            }
+        }
+        scanner.close();
+    }
+}
