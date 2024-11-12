0161f473f22032b49795e4028862d781d9684437
aryan <122105281+golu-aryan@users.noreply.github.com>
2024-11-12 13:21:16 +0530

    java

--- If-Else/waitlistRac.java
+++ If-Else/waitlistRac.java
@@ -1,10 +1,5 @@
 import java.util.Scanner;
 
-//Waitlist and RAC Confirmation
-//Write a Java program that takes the waitlist number as input and checks if the ticket can be confirmed:
-//If the waitlist number is less than or equal to 10, print "RAC Confirmed."
-//If the waitlist number is greater than 10 but less than or equal to 50, print "Waitlist, high chance of 
-//confirmation."Otherwise, print "Waitlist, low chance of confirmation."
 public class waitlistRac {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
