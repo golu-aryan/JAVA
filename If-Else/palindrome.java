9f9356502f7c2c1bf91a5c2b65e9933e1425689c
aryan <122105281+golu-aryan@users.noreply.github.com>
2024-11-12 13:20:35 +0530

    java

--- If-Else/palindrome.java
+++ If-Else/palindrome.java
@@ -6,16 +6,17 @@ public class palindrome {
     public static void main(String[] args) {
         Scanner t = new Scanner(System.in);
         int n = t.nextInt();
+        int original = n;
         int rev = 0, r;
         while (n > 0) {
             r = n % 10;
             rev = rev * 10 + r;
             n = n / 10;
         }
-        if (n == rev) {
-            System.out.println("The given no is palindrome" + rev);
+        if (original == rev) {
+            System.out.println("The given no is palindrome " + original);
         } else {
-            System.out.println("The given no is not palindrome" + rev);
+            System.out.println("The given no is not palindrome " + original);
         }
         t.close();
     }
