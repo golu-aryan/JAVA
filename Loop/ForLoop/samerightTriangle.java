65ef336595615418a03e504fed768c9e8bef3d2c
aryan <122105281+golu-aryan@users.noreply.github.com>
2024-11-20 17:24:32 +0530

    java

---
+++ Loop/ForLoop/samerightTriangle.java
@@ -0,0 +1,13 @@
+public class samerightTriangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
