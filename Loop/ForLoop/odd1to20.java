2f9fed89a393a256a6d0501296b9bbda62034d5d
aryan <122105281+golu-aryan@users.noreply.github.com>
2024-11-21 16:24:59 +0530

    java

---
+++ Loop/ForLoop/odd1to20.java
@@ -0,0 +1,9 @@
+public class odd1to20 {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 20; i++){
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
