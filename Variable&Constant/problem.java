b656656ffe8d06f9c5290f8361f308e0c9d5dc9e
aryan <122105281+golu-aryan@users.noreply.github.com>
2025-01-15 00:26:16 +0530

    Java

--- Variable&Constant/problem.java
+++
@@ -1,8 +0,0 @@
-public class problem{
-    public static void main(String[] args) {
-        int x = 5;
-        int y = 10;
-        int result = x++ + ++y -(y-- * 2);
-        System.out.println(result);
-    }
-}
