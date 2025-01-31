56940c1a0ebc577588f8301d808520120397f6d7
aryan <122105281+golu-aryan@users.noreply.github.com>
2025-01-31 19:07:59 +0530

    Java

---
+++ Array/1D_Array/arraywithforEach.java
@@ -0,0 +1,8 @@
+public class arraywithforEach {
+    public static void main(String[] args) {
+        int[] numbers = { 2, 3, 4, 5, 6, 7, 10 };
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
