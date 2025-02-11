4a5021cffb27dbc4dea806c1d0c01ab1b17194a9
aryan <122105281+golu-aryan@users.noreply.github.com>
2025-02-11 16:16:22 +0530

    Java

---
+++ Variable_Constant/operator.java
@@ -0,0 +1,11 @@
+/*
+what will be the output of the following expression float a = 7/4 * 9/2.
+ */
+public class operator {
+    public static void main(String[] args) {
+        float a = 7/4.0f * 9/2.0f; 
+
+        System.out.println(a);
+    }
+    
+}
