c55b5c20489a427bbeff0c6ca7f71b5ee6ea4eaf
aryan <122105281+golu-aryan@users.noreply.github.com>
2025-02-15 15:17:38 +0530

    Java

--- String/demo.java
+++
@@ -1,9 +0,0 @@
-public class demo {
-    public static void main(String[] args) {
-       
-        String str = "Aryan Aprajita";
-        int l = str.length();
-        System.out.println(l);
-        System.out.println(str.toUpperCase());
-    }
-}
