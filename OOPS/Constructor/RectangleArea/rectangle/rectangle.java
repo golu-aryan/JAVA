9251418e5eb175a60457156b96a56ffa9331f0f2
aryan <122105281+golu-aryan@users.noreply.github.com>
2025-03-17 11:55:08 +0530

    Java

---
+++ OOPS/Constructor/RectangleArea/rectangle/rectangle.java
@@ -0,0 +1,20 @@
+package JAVA.OOPS.Constructor.RectangleArea.rectangle;
+
+public class rectangle {
+    private int length;
+    private int width;
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public int getLength() {
+        return length;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public int getWidth() {
+        return width;
+    }
+}
