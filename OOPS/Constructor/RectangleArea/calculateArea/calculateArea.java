0cc0bc415c180942256d41b825e722598783d874
aryan <122105281+golu-aryan@users.noreply.github.com>
2025-03-17 11:54:34 +0530

    Java

---
+++ OOPS/Constructor/RectangleArea/calculateArea/calculateArea.java
@@ -0,0 +1,7 @@
+package JAVA.OOPS.Constructor.RectangleArea.calculateArea;
+import JAVA.OOPS.Constructor.RectangleArea.rectangle.rectangle;
+public class calculateArea {
+    public int calculate(rectangle rectangle) {
+        return rectangle.getLength() * rectangle.getWidth();
+    }
+}
