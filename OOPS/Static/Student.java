f253cae041e819fbf055a6af83bf839777a5bf4f
aryan <122105281+golu-aryan@users.noreply.github.com>
2025-04-06 14:19:33 +0530

    Java

--- OOPS/Static/Student.java
+++ OOPS/Static/Student.java
@@ -1,19 +1,11 @@
-//Parameterized Constructor
-public class Student {  
-        int id;
-        String name;
-        static String cName = "Batman";
-    Student(int i,String n){   //Constructor is a block of codes similar to method.It is called when an instance of the class is created.At the time of calling the constructor, memory for the object is allocated in the memory. It is a special type of method that is used 
-        id = i; 
-        name = n;
-    }
-    void display(){
-        System.out.println(id+" "+name+" "+cName);
-    }
+package JAVA.OOPS.Static;
+
+public class Student {
+    static String collegeName;
+
     public static void main(String[] args) {
-        Student s1 = new Student(111, "Aryan");
-        Student s2 = new Student(222,"Karan");
-        s1.display();
-        s2.display();
+        collegeName = "Institute of Technology!";
+
+        System.out.println("College Name :-> " + collegeName);
     }
 }
